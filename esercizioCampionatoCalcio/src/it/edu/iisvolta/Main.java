2fa4e38932d8537ca09a90e111d27ae3f418c9b4
barbe <barbe@Mikko2>
2023-03-31 20:59:25 +0200

    codice completato

--- esercizioCampionatoCalcio/src/it/edu/iisvolta/Main.java
+++ esercizioCampionatoCalcio/src/it/edu/iisvolta/Main.java
@@ -10,13 +10,24 @@ public class Main {
 		c.getElencoSquadre().add(new squadra("Napoli"));
 		c.getElencoSquadre().add(new squadra("Roma"));
 		c.getElencoSquadre().add(new squadra("Lazio"));
+		c.getElencoSquadre().add(new squadra("Fanfulla Lodi"));
 		
 		c.generaCalendario();
 		
 		for(partita p: c.getElencoPartite())
+		{
+			p.simulaPartita();
 			System.out.println(
 					p.getSquadraCasa().getNome() +" - " +
-					p.getSquadraFuori().getNome());
+					p.getSquadraFuori().getNome() + " : " +
+					p.getGolCasa() + " - " +
+					p.getGolFuori());
+		}
+		
+		System.out.println("\n\nCLASSIFICA: ");
+		for(squadra s: c.getElencoSquadre())
+			System.out.println(s.getNome() + ": " +s.getPunti());
+		
 	}
 
 }
